7a5ae07cf3a00ec351df6d591b445a6759c17519
Jenny Dolan <brightvibesrock@gmail.com>
2024-09-20 01:41:30 -0400

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -56,13 +56,9 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] sm:text-2xl mb-6 lg:text-2xl py-0.5">
-<<<<<<< HEAD
-            During the day, I lead teams that deliver platform capabilities that help internal software engineering organizations to build performant, stable, and reliable digital experiences for their customers. 
+            I lead teams that deliver platform capabilities that help internal software engineering organizations to build performant, stable, and reliable digital experiences for their customers. 
             
             Also, I am passionate about a new venture called NIFTY (Neat Innovations For Transforming You). I am partnering with the Maine Department of Labor to develop a curriculum that trains individuals with disabilities to create personal marketing platforms for job seekers. In today's job market, having an online presence is essential to landing interviews. I am excited to help this workforce develop the skills needed to succeed in helping others.
-=======
-          My full time job is working as a technical leader to deliver platform capabilities which enable internal software engineering organizations to deliver performant, stable, and reliable experiences into the hands of digital customers. I am also super passionate about a new venture called "NIFTY". Its basically job coaching people with disabilities so they have the skills to create personal sites for job seekers. NIFTY stands for "Neat Innovations For Transforming You".
->>>>>>> c0494c776c6bd0ce1b1f997a4c461944684300c0
           </p>
           <div>
             <Link
